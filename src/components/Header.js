07c04f8d582b4b1fcf42ed9b919532d16c951268
yopipi <ysk0420kbt@gmail.com>
2022-10-23 17:03:59 +0900

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,29 @@ import React, {useContext, useState} from 'react';
 import dig from "object-dig"
 import { signInWithGoogle, logOut } from '../service/firebase';
 import {AuthContext} from "../provider/AuthProvider"
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(() => ({
+  toolbar: {
+    justifyContent: 'space-between'
+  },
+  button: {
+    color: '#FFF',
+  },
+  appbar: {
+    backgroundColor: "#f3b456",
+  }
+}));
 
 const Header = () => {
+
+  const classes = useStyles();
+
   const currenUser = useContext(AuthContext);
   console.log(currenUser);
   
@@ -11,26 +32,46 @@ const Header = () => {
   const buttonRender = () => {
     let buttonDom
     if( dig(currenUser, 'currentUser', 'uid') ){
-      // ユーザー名の取得
-      let name = dig(currenUser, 'currentUser', 'bc', 'displayName')
       buttonDom = (
       <>
-      <button onClick={logOut}>ログアウト
-      </button>
-      <p>{name}</p>
+      <Button className={classes.button} onClick={logOut}>Log-out
+      </Button>
       </>
       )
     }else{
-      buttonDom = <button onClick={signInWithGoogle}>ログイン</button>
+      buttonDom = <Button className={classes.button} onClick={signInWithGoogle}>Log-in</Button>
     }
     return buttonDom
   }
 
+  const nameRender = () => {
+    let nameDom;
+    if( dig(currenUser, 'currentUser', 'uid') ){
+      let name = dig(currenUser, 'currentUser', 'bc', 'displayName');
+      nameDom = (
+        <>{name}</>
+      )
+    }else{
+      nameDom = <p></p>
+    }
+    return nameDom;
+  }
+
+
   return(
-    <header>
-      ヘッダー
-      {buttonRender()}
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static" className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          PiggyWallet
+        </Typography>
+        <Typography variant="subtitle2" component="div" sx={{ flexGrow: 1 }}>
+          {nameRender()}'s wallet
+        </Typography>
+        <Button color="inherit">{buttonRender()}</Button>
+      </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
 
